a7db60d3dab310bd2ecf8783a002e70139ee6ea7
sured415 <wtghnh@gmail.com>
2018-12-03 17:40:04 +0900

    mod main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@
 #include <string.h>
 #include <openssl/ssl.h>		// ssl
 #include <openssl/tls1.h>
+#define GLOG_NO_ABBREVIATED_SEVERITIES
+#include <glog/logging.h>
 
 #define SERVER_MODE		0
 #define CLIENT_MODE		1
@@ -17,28 +19,23 @@
 
 using namespace std;
 
-char *server_hello = "HTTP/1.1 200 OK\nContent-Length: 5\n\nHello\n\0";
-char *req_to_server = "GET / HTTP/1.1\nHost: test.com\n\0";
 const char* server_name;
-const char* host_name = 0;
 SSL_CTX* ctx_s;
+SSL_CTX* ctx_c;
 
 struct ssl_obj {
 	SOCKET server_s;
 	SOCKET client_s;
-	SSL *first_ssl;
-	SSL *second_ssl;
+	SSL *first_ssl{ nullptr };
+	SSL *second_ssl{ nullptr };
 }objs;
 
-sockaddr_in set_sockaddr(int port, char* ip);
-SOCKET connect_tcp(int port, char* ip);
+sockaddr_in set_sockaddr(int port, int flag);
+SOCKET connect_tcp(int port, int flag);
 void create_crt(SSL_CTX* ctx);
 void get_servername(SSL* ssl);
-int connect_ssl_vServer(SOCKET s);
-void connect_ssl_vClient(SOCKET s);
-int com_ssl(SSL* src, SSL* dst);
-unsigned WINAPI connect_ssl_vServer2(void* obj);
-unsigned WINAPI connect_ssl_vClient2(void* obj);
+int connect_ssl();
+unsigned WINAPI com_ssl(void* obj);
 
 int main(int argc, char* argv[]) {
 	if (argc != 1) {
@@ -46,32 +43,42 @@ int main(int argc, char* argv[]) {
 	}
 
 	objs.server_s = connect_tcp(443, SERVER_MODE);
-//	objs.client_s = connect_tcp(4433, "172.31.12.162");
-	objs.client_s = connect_tcp(4433, "172.31.5.59");
-	HANDLE server_thread = (HANDLE)_beginthreadex(NULL, 0, connect_ssl_vServer2, (void*)&objs, 0, NULL);
-	HANDLE client_thread = (HANDLE)_beginthreadex(NULL, 0, connect_ssl_vClient2, (void*)&objs, 0, NULL);
-	WaitForSingleObject(server_thread, INFINITE);
-	WaitForSingleObject(client_thread, INFINITE);
+	connect_ssl();
 
+	WSACleanup();
 	return 0;
 }
 
-sockaddr_in set_sockaddr(int port, char* ip) {
+sockaddr_in set_sockaddr(int port, int flag) {
 	sockaddr_in addr;
 	memset(&addr, 0, sizeof(addr));
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(port);
-	if (ip == SERVER_MODE) addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	else inet_pton(AF_INET, ip, &addr.sin_addr.s_addr);
+	if (flag == SERVER_MODE) addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	else {
+		struct addrinfo* result;
+		char c_port[5] = { 0, };
+		sprintf_s(c_port, "%d", port);
 
+		int res = getaddrinfo(server_name, c_port, NULL, &result);
+		if (res != 0) {
+			cout << "getaddrinfo fail" << endl;
+		}
+		else {
+			sockaddr_in* host_name;
+			host_name = (sockaddr_in *)result->ai_addr;
+			addr.sin_addr.s_addr = host_name->sin_addr.s_addr;
+		}
+		freeaddrinfo(result);
+	}
 	return addr;
 }
 
-SOCKET connect_tcp(int port, char* ip) {
+SOCKET connect_tcp(int port, int flag) {
 	WSADATA wsadata;
 	SOCKET s;
 	sockaddr_in addr;
-	addr = set_sockaddr(port, ip);
+	addr = set_sockaddr(port, flag);
 
 	if (WSAStartup(MAKEWORD(2, 2), &wsadata) != 0) {
 		cout << "Socket reset error" << endl;
@@ -84,11 +91,10 @@ SOCKET connect_tcp(int port, char* ip) {
 		return -1;
 	}
 
-	if (ip == SERVER_MODE) {
+	if (flag == SERVER_MODE) {
 		if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) == SOCKET_ERROR) {	// Bind
 			cout << "Bind error" << endl;
 		}
-
 		if (listen(s, 5) == SOCKET_ERROR) {										// Listen
 			cout << "Listen error" << endl;
 		}
@@ -133,77 +139,15 @@ void get_servername(SSL* ssl) {
 	create_crt(ctx_s);
 }
 
-int connect_ssl_vServer(SOCKET s) {
-	sockaddr_in client_addr;
-	int c_addr_size = sizeof(client_addr);
-	char c_message[MAXBUF] = { "\0", };
-
-	SSL_library_init();				// OpenSSL init
-	ctx_s = SSL_CTX_new(TLSv1_2_server_method());				// TLSv1.2 context create
-
-	while (1) {
-		SOCKET c = accept(s, (struct sockaddr *)&client_addr, &c_addr_size);
-		if (c == SOCKET_ERROR) {															// Accept
-			cout << "Accept error" << endl;
-		}
-
-		SSL *ssl = SSL_new(ctx_s);
-		SSL_CTX_set_tlsext_servername_callback(ctx_s, get_servername);
-		SSL_set_fd(ssl, (int)c);
-		SSL_accept(ssl);
-
-		while (SSL_read(ssl, c_message, sizeof(c_message)) > 0) {
-			cout << c_message << endl;
-			memset(c_message, '\0', sizeof(c_message));
-			SSL_write(ssl, server_hello, strlen(server_hello));
-		}
-
-		SSL_free(ssl);
-	}
-	SSL_CTX_free(ctx_s);
-	closesocket(s);
-	WSACleanup();
-	return 0;
-}
-
-void connect_ssl_vClient(SOCKET s) {
-	char rev_message[MAXBUF] = { "\0", };
-
-	SSL_library_init();				// OpenSSL init
-	ctx_s = SSL_CTX_new(TLSv1_2_client_method());				// TLSv1.2 context create
-
-	while (1) {
-		SSL *ssl = SSL_new(ctx_s);
-		SSL_set_fd(ssl, (int)s);
-		if (SSL_connect(ssl)) {
-			SSL_write(ssl, req_to_server, strlen(req_to_server));
-			while (SSL_read(ssl, rev_message, sizeof(rev_message)) > 0) {
-				cout << rev_message << endl;
-				memset(rev_message, '\0', sizeof(rev_message));
-			}
-		}
-		else {
-			cout << "ssl connect fail" << endl;
-			break;
-		}
-		SSL_free(ssl);
-	}
-	SSL_CTX_free(ctx_s);
-	closesocket(s);
-	WSACleanup();
-}
-
-unsigned WINAPI connect_ssl_vServer2(void* obj) {
-//	objs = *(ssl_obj*)obj;
-
+int connect_ssl() {
+	LOG(INFO) << "start connect";
 	sockaddr_in client_addr;
 	int c_addr_size = sizeof(client_addr);
 
-	SSL_library_init();				// OpenSSL init
 	ctx_s = SSL_CTX_new(TLSv1_2_server_method());				// TLSv1.2 context create
-
+	SOCKET c;
 	while (1) {
-		SOCKET c = accept(objs.server_s, (struct sockaddr *)&client_addr, &c_addr_size);
+		c = accept(objs.server_s, (struct sockaddr *)&client_addr, &c_addr_size);
 		if (c == SOCKET_ERROR) {															// Accept
 			cout << "Accept error" << endl;
 		}
@@ -211,58 +155,52 @@ unsigned WINAPI connect_ssl_vServer2(void* obj) {
 		objs.first_ssl = SSL_new(ctx_s);
 		SSL_CTX_set_tlsext_servername_callback(ctx_s, get_servername);
 		SSL_set_fd(objs.first_ssl, (int)c);
-		SSL_accept(objs.first_ssl);
-
-		com_ssl(objs.first_ssl, objs.second_ssl);
-		
-		SSL_free(objs.first_ssl);
-		closesocket(c);
+		int res = SSL_accept(objs.first_ssl);
+		LOG(INFO) << "Server SSL_accept return = " << res;
+		if (res == 1) {
+			objs.client_s = connect_tcp(4433, CLIENT_MODE);
+			ctx_c = SSL_CTX_new(TLSv1_2_client_method());
+			objs.second_ssl = SSL_new(ctx_c);
+			SSL_set_fd(objs.second_ssl, (int)objs.client_s);
+			if (SSL_connect(objs.second_ssl) == 1) {
+				HANDLE ps_thread = (HANDLE)_beginthreadex(NULL, 0, com_ssl, (void*)&objs, 0, NULL);
+				ssl_obj revers_objs;
+				revers_objs.first_ssl = objs.second_ssl;
+				revers_objs.second_ssl = objs.first_ssl;
+				HANDLE pc_thread = (HANDLE)_beginthreadex(NULL, 0, com_ssl, (void*)&revers_objs, 0, NULL);
+			}
+		}
 	}
+	SSL_free(objs.first_ssl);
+	SSL_free(objs.second_ssl);
+	closesocket(c);
+
 	SSL_CTX_free(ctx_s);
-	WSACleanup();
+	SSL_CTX_free(ctx_c);
+	LOG(INFO) << "stop  connect_ssl";
 	return 0;
 }
 
-unsigned WINAPI connect_ssl_vClient2(void* obj) {
-//	objs = *(ssl_obj*)obj;
-	SSL_library_init();				// OpenSSL init
-	ctx_s = SSL_CTX_new(TLSv1_2_client_method());				// TLSv1.2 context create
-
-	while (1) {
-		objs.second_ssl = SSL_new(ctx_s);
-		SSL_set_fd(objs.second_ssl, (int)objs.client_s);
-
-		/*
-		if (SSL_connect(objs.second_ssl)) {
-			com_ssl(objs.second_ssl, objs.first_ssl);
-		}
-		else {
-			cout << "ssl connect fail" << endl;
-		}
-		*/
-		SSL_connect(objs.second_ssl);
-		com_ssl(objs.second_ssl, objs.first_ssl);
-		SSL_free(objs.second_ssl);
-	}
-	SSL_CTX_free(ctx_s);
-	closesocket(objs.client_s);
-	WSACleanup();
-	return 0;
-}
-
-int com_ssl(SSL* src, SSL* dst) {
+unsigned WINAPI com_ssl(void* obj) {
+	ssl_obj _obj = *(ssl_obj*)obj;
+	LOG(INFO) << "com_ssl2 " << (void*)_obj.first_ssl << " " << (void*)_obj.second_ssl;
 	char ssl_data[MAXBUF] = { "\0", };
 
-	while (SSL_read(src, ssl_data, sizeof(ssl_data)) > 0) {
-		cout << ssl_data << endl << endl;
-		/*
-		if (SSL_write(a.first_ssl, server_hello, strlen(server_hello)) > 0) {
-			cout << "send success" << endl;
+	while (1) {
+		LOG(INFO) << "bef SSL_read ssl=" << (void*)_obj.first_ssl;
+		int res = SSL_read(_obj.first_ssl, ssl_data, sizeof(ssl_data));
+		LOG(INFO) << "SSL_read res=" << res;
+		if (res > 0) {
+			cout << ssl_data << endl << endl;
+			LOG(INFO) << "bef SSL_write =" << (void*)_obj.second_ssl;
+			res = SSL_write(_obj.second_ssl, ssl_data, strlen(ssl_data));
+			LOG(INFO) << "SSL_write res = " << res;
 		}
-		else cout << "send fail" << endl;*/
-		SSL_write(dst, ssl_data, strlen(ssl_data));
+		else {
+			cout << SSL_get_error(_obj.first_ssl, res) << endl;
+			break;
+		}
 		memset(ssl_data, '\0', sizeof(ssl_data));
 	}
-
 	return 0;
 }
